b1f182ba59737c85ae57e51bef70d323255c046b
aorinkaanendya <chazanahaorin@gmail.com>
2020-08-07 16:55:56 +0700

    installasi

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+       return "NIS : 3103118021
+                Nama : Aorinka Anendya Chazanah
+                Gender : Perempuan
+                Phone : 085876062320
+                Kelas : XII RPL 1";
+    }
+}
