82343ab86514e982f61b337697f0bf97b96c80a1
aorinkaanendya <chazanahaorin@gmail.com>
2020-10-08 08:07:07 +0700

    instalasi

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Author;
+use Validator;
+use App\User;
 
 class AuthorController extends Controller
 {
@@ -21,6 +24,7 @@ class AuthorController extends Controller
         }else{
             return response(['message' => 'No data to be displayed', 'data' => null], 404);
         }
+        
     }
 
     /**
@@ -30,6 +34,7 @@ class AuthorController extends Controller
      */
     public function create()
     {
+
         //
     }
 
@@ -41,14 +46,16 @@ class AuthorController extends Controller
      */
     public function store(Request $request)
     {
-        return Author::create([
+        // id, name, date_of_birth, place_of_birth, gender, email, hp , create_at, update_at.
+        $data = Author::create([
             "name" => $request->input('name'),
             "date_of_birth" => $request->input('date_of_birth'),
             "place_of_birth" => $request->input('place_of_birth'),
             "gender" => $request->input('gender'),
             "email" => $request->input('email'),
-            "hp" => $request->input('hp')
+            "hp" => $request->input('hp'),
         ]);
+
         return response(['message' => 'Create data success', 'data' => $data], 201);
     }
 
@@ -60,12 +67,14 @@ class AuthorController extends Controller
      */
     public function show($id)
     {
-        //$author = Author::find($id);
+        //
+        $author = Author::find($id);
         if ($author) {
             return response(['message' => 'Show data success', 'data' => $author], 200);
         }else{
             return response(['message' => 'data not found', 'data' => null], 404);
         }
+        
     }
 
     /**
@@ -88,6 +97,7 @@ class AuthorController extends Controller
      */
     public function update(Request $request, $id)
     {
+        //
         $author = Author::find($id);
         if ($author) {
             $data = $author->update([
@@ -112,6 +122,7 @@ class AuthorController extends Controller
      */
     public function destroy($id)
     {
+        //
         $author = Author::find($id);
         if ($author) {
             $data = Author::destroy($id);
@@ -120,5 +131,4 @@ class AuthorController extends Controller
             return response(['message' => 'no data to delete', 'data' => null], 404);
         }
     }
-    
 }
